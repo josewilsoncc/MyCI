40894ec1307a9a8dc222a7e80da834f763d1e38b
user <user@desarrollo7>
2014-12-29 08:52:05 -0500

    bootstrap helper resolve #15

---
+++ application/helpers/bootstrap_helper.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Este Helper es el encargado de ejecutar las acciones necesarias al inicio
+ * de la ejecución de la aplicación como lo son la carda de Helpers, Libraries
+ * y demas.
+ * 
+ * @author Jose Wilson Capera Castaño, josewilsoncc@hotmail.com
+ * @date 2014/12/29
+ */
+
+$ci = & get_instance();
+
+//Carga de Helpers de CI
+$ci->load->helper(array(
+  'form',
+  'html',
+  'url'
+));
+
+//Carga de libraries de CI
+$ci->load->library(array(
+  'form_validation'
+));
+
+//Carga de drivers de CI
+$ci->load->driver(array(
+  'session'
+));
+
+//Carga de Helpers del framework
+$ci->load->helper(array(
+  'essential',
+  'elements'
+));
